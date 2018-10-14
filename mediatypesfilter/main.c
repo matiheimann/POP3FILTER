9ada6cc1ff4b7b373d6794b63ff93c34aabbabaa
matiheimann <mheimann@itba.edu.ar>
2018-10-13 23:28:48 -0300

    Fix

--- mediatypesfilter/main.c
+++ mediatypesfilter/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char const *argv[], char** envp)
+int main(int argc, char const *argv[])
 {
 	
 	char* filterMessage = getenv("FILTER_MSG");
