2fc600dbb07908882abb83eadd502ec561ec4e00
matiheimann <mheimann@itba.edu.ar>
2018-10-15 17:11:44 -0300

    Media type validation finished

--- pop3filter/mediatypes.c
+++ pop3filter/mediatypes.c
@@ -7,11 +7,8 @@ int isValidMediaType(char* mediatype)
 {
 	int i = 0;
 	int slash = 0;
-
-	if(strcmp("*/*", mediatype) == 0)
-	{
-		return 1;
-	}
+	int dotComma = 0;
+	int equals = 0;
 
 	while(mediatype[i] != 0)
 	{
@@ -23,13 +20,42 @@ int isValidMediaType(char* mediatype)
 				{
 					return (mediatype[i+1] == 0 && mediatype[i-1] == '/') ? 1 : 0;
 				}
-				return 0;
+				else if(mediatype[i] == ';')
+				{
+					if(mediatype[i-1] != '/')
+					{
+						dotComma = 1;
+					}
+					else
+					{
+						return 0;
+					}
+				}
+				else if(mediatype[i] == '=')
+				{
+					if(dotComma != 0 &&  mediatype[i-1] != ';')
+					{
+						equals = 1;
+					}
+					else
+					{
+						return 0;
+					}
+				}
+				else
+				{
+					return 0;
+				}
 			}
 			else
 			{
-				if(mediatype[i] == '/')
+				if(mediatype[i] == '/' && i != 0)
 				{
 					slash = 1;
+				}
+				else if(mediatype[i] == '*' && i == 0)
+				{
+					return (strcmp("*/*", mediatype) == 0) ? 1 : 0;
 				}
 				else
 				{
@@ -46,6 +72,16 @@ int isValidMediaType(char* mediatype)
 	}
 
 	if(mediatype[i-1] == '/')
+	{
+		return 0;
+	}
+
+	if(dotComma != 0 && equals == 0)
+	{
+		return 0;
+	}
+
+	if(equals != 0 && mediatype[i-1] == '=')
 	{
 		return 0;
 	}
